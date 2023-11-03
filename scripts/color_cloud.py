2070df3ff0c704293e7b6b3dfee6a15b83dd0039
zyx <yuxuan.zeng@sjtu.edu.cn>
2023-11-03 21:35:01 +0800

    mid360内外参以及上色

---
+++ scripts/color_cloud.py
@@ -0,0 +1,196 @@
+import open3d as o3d
+import numpy as np
+import warnings
+import cv2 
+import os
+from scipy.spatial.transform import Rotation as R
+warnings.filterwarnings("ignore",category=RuntimeWarning)
+
+def load_pcd(pointcloud_file):
+    pcd_origin = o3d.io.read_point_cloud(pointcloud_file)
+    pcd = o3d.geometry.PointCloud()
+    points = np.asarray(pcd_origin.points)
+    pcd.points = o3d.utility.Vector3dVector(points)
+    return pcd
+
+
+def get_R_and_T(Trans_matrix):
+    if Trans_matrix.shape[0]!=4:
+        print("False")
+    R = np.zeros((3,3))
+    T = np.zeros((3,1))
+    R = Trans_matrix[0:3,0:3]
+    T = Trans_matrix[0:3,3:4]
+    rvec = cv2.Rodrigues(R)[0]
+    rvec = np.float64([rvec[0,0],rvec[1,0], rvec[2,0]])
+    tvec = np.float64([T[0,0],T[1,0], T[2,0]])
+    return R,rvec, tvec
+
+
+def get_inverse_T(Trans_origin):
+    R = Trans_origin[:3, :3]
+    T = Trans_origin[:3, 3]
+    R_transpose = R.T
+    T_inv = -np.dot(R_transpose, T)
+    Trans_inverse = np.eye(4)
+    Trans_inverse[:3, :3] = R_transpose
+    Trans_inverse[:3, 3] = T_inv
+    return Trans_inverse
+
+
+def colorCloud(pcd, img_file, rvec, tvec, camera_intrinsic, distCoeffs,point_colors_bgr):
+    image = cv2.imread(img_file) # load the image
+    points = np.asarray(pcd.points)
+    num_points = points.shape[0]
+    point_matrix = np.ones((num_points, 4))
+    point_matrix[:, :3] = points
+    point_2d, _ = cv2.projectPoints(points, rvec, tvec, camera_intrinsic, distCoeffs)
+    point_pixel_coords = point_2d.astype(int)
+    
+    distance = []
+    for point in points:
+        dis = np.sqrt(np.power(point[0],2)+np.power(point[1],2)+np.power(point[2],2))
+        distance.append(dis)
+
+    result_image = np.copy(image)
+    i = 0
+    repeat_matrix = np.empty((result_image.shape[0],result_image.shape[1]),dtype=object)
+    for i in range(result_image.shape[0]):
+        for j in range(result_image.shape[1]):
+            repeat_matrix[i][j] = []
+    i = 0
+    repeat_index = []
+    for point in point_pixel_coords:
+        x = point[0][0]
+        y = point[0][1]
+        if 0 <= y < result_image.shape[0] and 0 <= x < result_image.shape[1] and point_matrix[i,0]>=0 and point_matrix[i,2]>=0 and distance[i]<=25:
+            # if [y,x] not in repeat_index:
+            #     repeat_index.append([y,x])
+            #     repeat_matrix[y,x].append(i)
+            # else:
+            #     if distance[repeat_matrix[y,x][0]]>distance[i]:
+            #         repeat_matrix[y,x][0] = i
+            #     # print("repeat",[y,x])
+            #     # print(repeat_matrix[y,x])
+            #     pass
+            if sum(point_colors_bgr[i]) <= 3:
+                point_colors_bgr[i] = image[y, x]
+        i += 1
+    # for y in range(result_image.shape[0]):
+    #     for x in range(result_image.shape[1]):
+    #         if repeat_matrix[y][x]:
+    #            point_colors_bgr[repeat_matrix[y][x][0]] = image[y, x]
+        
+    # for y in range(result_image.shape[0]):
+    #     for x in range(result_image.shape[1]):
+    #         if repeat_matrix[y][x]:
+    #             index_min = repeat_matrix[y][x][0]
+    #             if len(repeat_matrix[y][x])-1:
+    #                 distance_min = 1000
+    #                 for index in repeat_matrix[y][x]:
+    #                     if distance[index]<distance_min:
+    #                         distance_min = distance[index]
+    #                         index_min = index
+    #             point_colors_bgr[index_min] = image[y, x]
+                
+    
+
+    return point_colors_bgr
+
+
+def color_pic(pcd, img_file, rvec, tvec, camera_intrinsic, distCoeffs):
+    image = cv2.imread(img_file) # load the image
+    points = np.asarray(pcd.points)
+    num_points = points.shape[0]
+    point_matrix = np.ones((num_points, 4))
+    point_matrix[:, :3] = points  
+    point_2d, _ = cv2.projectPoints(points, rvec, tvec, camera_intrinsic, distCoeffs)
+    point_pixel_coords = point_2d.astype(int)
+
+    result_image = np.copy(image)
+    point_colors_bgr = np.zeros((point_pixel_coords.shape[0], 3), dtype=np.uint8)
+    i = 0
+    for point in point_pixel_coords:
+        x = point[0][0]
+        y = point[0][1]
+        if 0 <= y < result_image.shape[0] and 0 <= x < result_image.shape[1] and point_matrix[i,0]>=0:
+            result_image[y, x] = [0, 0, 0]
+        i += 1
+    return result_image
+
+
+def key_frame_extract(key_frame_file):
+    rotation_matrixs = []
+    T = []
+    Transformations = []
+    Times = []
+    with open(key_frame_file, 'r' ) as file:
+        lines = file.readlines()
+    for line in lines:
+        elements = line.strip().split()
+        Time = float(elements[0])
+        q = np.array([elements[1], elements[2], elements[3], elements[4]])
+        t = np.array([[elements[5]], [elements[6]], [elements[7]]]).astype(float)
+        RT = R.from_quat(q)
+        RT = RT.as_matrix()
+        Transformation = np.zeros((4,4))
+        Transformation[0:3,0:3] = RT
+        Transformation[:,3:4] = [[float(elements[5])], [float(elements[6])], [float(elements[7])],[1.0]]
+        rotation_matrixs.append(RT)
+        T.append(t)
+        Transformations.append(Transformation)
+        Times.append(Time)
+    return Transformations,Times
+
+def get_img_list(Img_file):
+    all_objects = os.listdir(Img_file)
+    img_list = [file for file in all_objects if file.lower().endswith(('.jpg', '.jpeg', '.png', '.gif'))]
+    img_name = []
+    for img in img_list:
+        name = os.path.splitext(img)[0]
+        name = float(name)
+        img_name.append(name)
+    return img_list,img_name
+
+def get_img_name(img_list,img_name,stamp):
+    i=0
+    count = 0
+    former_delta = img_name[0]+stamp
+    for name in img_name:
+        delta = abs(name - stamp)
+        if delta < former_delta:
+            former_delta = delta
+            count = i
+        i += 1
+    return img_list[count]
+
+
+if __name__ == "__main__":
+    pointcloud_file = 'F:/data/calibration/calibration_in/1.pcd'
+    img_file = 'F:/data/calibration/1.jpg'
+    Img_path = 'F:/data/0814/pic_imr/pictures'
+    
+    img_list,img_name = get_img_list(Img_path)
+    
+    filename = Img_path+'/'+get_img_name(img_list,img_name,1691140902.9809)
+    print(filename)
+    
+
+    # camera_intrinsic = np.float64([
+    #     [918.6080, 0.0, 631.37719],
+    #     [0.0, 915.973022, 356.666],
+    #     [0.0, 0.0, 1.0]
+    # ])
+    # distCoeffs = np.float64([0.0, 0.0, 0.0, 0.0, 0.0])
+
+    # RT = np.float64([
+    #     [-0.346889,-0.937895,0.00461545],
+    #     [0.251673,-0.0978218,-0.962856],
+    #     [0.903509,-0.332842,0.269976],
+    # ])
+    # tvec = np.float64([0.180297, 0.240911, -0.0592223])
+
+    # rvec = cv2.Rodrigues(RT)[0]
+    # rvec = np.float64([rvec[0,0],rvec[1,0], rvec[2,0]])
+    # pcd = colorCloud(pointcloud_file,img_file,rvec, tvec, camera_intrinsic, distCoeffs)
+    # o3d.visualization.draw_geometries([pcd])
---
+++ scripts/imr_color_once.py
@@ -0,0 +1,84 @@
+'''
+注释？
+'''
+import open3d as o3d
+import numpy as np
+import cv2 
+import color_cloud
+import copy
+
+# Init the file name
+pointcloud_file = 'data/3.pcd'
+img_file = 'data/1.png'
+key_frame = 'data/key_frame.txt'
+
+# Init the parameters
+camera_intrinsic = np.float64([
+    [911.3619384, 0.0, 652.80969238],
+    [ 0.0, 911.00201416, 364.7060852],
+    [0.0, 0.0, 1.0]
+])
+distCoeffs = np.float64([0.0, 0.0, 0.0, 0.0, 0.0])
+
+lidar_to_camera_extrinsic = np.float64([
+    [-0.3315105, -0.9428254, -0.0343649, 0.058404],
+    [ 0.2627532, -0.0572815, -0.9631612, -0.293531],
+    [ 0.9061245, -0.3283276,  0.2667198, 0.0122927],
+    [ 0, 0, 0, 1]
+])
+
+# lidar_to_camera_extrinsic = np.float64([
+#     [-0.305056, -0.951714, -0.0343655, 0.059404],
+#     [ 0.26425, -0.0499221, -0.963161, -0.293531],
+#     [ 0.914939, -0.302899,  0.26672, 0.0122927],
+#     [ 0, 0, 0, 1]
+# ])
+
+
+if __name__ == "__main__":
+    # Load the files with given parameters
+    pcd = color_cloud.load_pcd(pointcloud_file)
+    points = np.asarray(pcd.points)
+    num_points = points.shape[0]
+    point_colors_bgr = np.ones((num_points, 3), dtype=np.uint8)
+    Trans, Stamps= color_cloud.key_frame_extract(key_frame)
+    _,rvec,tvec = color_cloud.get_R_and_T(lidar_to_camera_extrinsic)
+
+    # Load the pose info to matrix for loop
+    
+    # pcd = o3d.io.read_point_cloud(pointcloud_file)
+    # pose = Trans[100]
+    # print(Stamps[100])
+    # pose_inverse = color_cloud.get_inverse_T(pose)
+    # pcd.transform(pose_inverse)
+    
+    # Choose function
+    if_pic = 0
+
+    # Project pcd onto the image, project only once
+    if if_pic:
+        result = color_cloud.color_pic(pcd,img_file,rvec, tvec, camera_intrinsic, distCoeffs)
+        cv2.imshow('Projected Points', result)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    
+    # Transform the color matrix from bgr to rgb so that pcd can be viewed normally
+    else:
+        point_colors_bgr= color_cloud.colorCloud(pcd,img_file,rvec, tvec, camera_intrinsic, distCoeffs,point_colors_bgr)
+        point_colors_rgb = point_colors_bgr[:, [2, 1, 0]]
+        point_colors_rgb = point_colors_rgb / 255
+        # # pcd.transform(pose)
+        pcd.colors = o3d.utility.Vector3dVector(point_colors_rgb)
+        FOR1 = o3d.geometry.TriangleMesh.create_coordinate_frame(size=15, origin=[0, 0, 0])
+        pcd2 = color_cloud.load_pcd("F:/data/mid360/2.pcd")
+        o3d.visualization.draw_geometries([FOR1,pcd])
+        
+    o3d.io.write_point_cloud("data/result/copy_of_fragment.pcd", pcd)
+    
+    # # View coordinate of pointcloud
+    # FOR1 = o3d.geometry.TriangleMesh.create_coordinate_frame(size=15, origin=[0, 0, 0])
+    # pcd_T = copy.deepcopy(pcd)
+    # pcd_T.transform(pose_inverse)
+    # o3d.visualization.draw_geometries([FOR1,pcd_T], window_name="常规变换",
+    #                           width=800,  # 窗口宽度
+    #                           height=600)
