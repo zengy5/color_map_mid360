c95c312d298cfe93facb4f8d9a379b384c2f5b18
zyx <yuxuan.zeng@sjtu.edu.cn>
2023-11-24 15:16:06 +0800

    imr信息

--- edgedetect.py
+++ edgedetect.py
@@ -1,9 +1,11 @@
 import cv2
 
-img1_path = "data/s0.png"
+img1_path = "data/1.png"
 img2_path = "data/s1.png"
+img3_path = "data/s2.jpg"
+img4_path = "data/s3.jpg"
 
-img = cv2.imread(img2_path,0)
+img = cv2.imread(img4_path,0)
 blurred = cv2.GaussianBlur(img,(15,15),0)
 gaussImg = cv2.Canny(blurred, 20, 70)
 cv2.imshow("Img",gaussImg)
